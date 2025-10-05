ec10e10744cc711da6a4d3a99de67c3209ad872f
atharva-potdar <atharvapotdar07@gmail.com>
2025-10-05 18:48:45 +0530

    feat: add contract importing

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,6 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity >=0.8.30 <0.9;
 
+// don't prefer this - it imports all contracts from 1 file
+// import "./SimpleStorage.sol";
+
+// prefer this
+import {SimpleStorage} from "./SimpleStorage.sol";
+
 contract StorageFactory {
-    
+
+    SimpleStorage[] public simpleStorages;
+
+    function createSimpleStorageContract() public {
+        simpleStorages.push(new SimpleStorage());
+    }
+
 }
